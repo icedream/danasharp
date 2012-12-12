0e1e894eefc7ec4c5325e2e8d9d07afb67abd5c2
Icedream <icedream2k9@die-optimisten.net>
2012-12-12 19:16:46 +0100

    I forgot this one ._.

---
+++ DanaSharp/Hostmask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace DanaSharp
+{
+    public class Hostmask
+    {
+        private string hostmask;
+        private Regex regex;
+
+        public Hostmask(string hostmask)
+        {
+            this.hostmask = hostmask;
+            this.regex = new Regex("^" + hostmask.Replace("?", ".?").Replace("*", ".*") + "$", RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        }
+
+        public bool IsMatch(string hostmask)
+        {
+            return this.regex.IsMatch(hostmask);
+        }
+    }
+}
